6f1b8c1ed8f52b79326a241a82c6423d30452686
nsingh <nsingh@pivotal.io>
2018-05-28 13:00:27 -0700

    Add tasks

---
+++ ci-base/tasks/build-fat-jar.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+base_dir=`pwd`
+app_dir=${base_dir}/git/${app_name}
+out_dir=${base_dir}/out
+
+echo "PWD=${base_dir}"
+
+mkdir -p ${base_dir}/.m2
+
+ln -s ${base_dir}/.m2 /root/.m2
+echo "=== ~/.m2/ contents ===="
+ls -la /root/.m2/
+echo "========================"
+
+cd $app_dir
+./mvnw clean package
+
+timestamp=`date -u +%Y%m%d%H%M`
+
+jar_name=$(basename `ls $app_dir/target/*.jar`)
+cp -a ${base_dir}/git/ci/docker/* ${out_dir}
+cp $app_dir/target/*.jar ${out_dir}/fatjar.jar
