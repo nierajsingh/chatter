e4d8ce1943aab8e8f7f98247b67c0969b8b79a75
nsingh <nsingh@pivotal.io>
2018-05-28 12:17:46 -0700

    Add basic greetings app for CI deployment

---
+++ basic-greetings/src/main/java/org/example/basic/greetings/demo/BasicGreetingsApplication.java
@@ -0,0 +1,12 @@
+package org.example.basic.greetings.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicGreetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicGreetingsApplication.class, args);
+	}
+}
